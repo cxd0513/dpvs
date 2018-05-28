5255af9274efb565caa35b9f51628ccc4b75f064
cxd0513 <caoxudong_sx@qiyi.com>
2018-05-28 18:01:02 +0800

    ./dpvs

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cp patch/dpdk-stable-17.05.2/*.patch dpdk-stable-17.05.2/
 cd dpdk-stable-17.05.2/
 patch -p 1 < 0001-PATCH-kni-use-netlink-event-for-multicast-driver-par.patch
 make config T=x86_64-native-linuxapp-gcc
-make
+make -j40
 export RTE_SDK=/root/dpvs/dpdk-stable-17.05.2
 
 
